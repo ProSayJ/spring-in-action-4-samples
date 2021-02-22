49a5c5f54bd384dcdfc3d14279b9a4d276b064bd
prosayj@gmail.com <15665662468@163.com>
2021-02-22 17:24:59 +0800

    feat:spring4-spring-web-view-rendering-thymeleaf test case
    feat:spring4-spring-web-view-rendering-thymeleaf test casefeat:add some test case

---
+++ chapter_06/spring4-spring-web-view-rendering-thymeleaf/src/main/java/prosayj/thinking/spring4/webviewthymeleaf/spittr/config/RootConfig.java
@@ -0,0 +1,30 @@
+package prosayj.thinking.spring4.webviewthymeleaf.spittr.config;
+
+import java.util.regex.Pattern;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+
+
+/**
+ * RootConfig
+ *
+ * @author yangjian
+ */
+@Configuration
+@Import(DataConfig.class)
+@ComponentScan(basePackages = {"prosayj.thinking.spring4.webviewthymeleaf"},
+        excludeFilters = {
+                @Filter(type = FilterType.CUSTOM, value = RootConfig.WebPackage.class)
+        })
+public class RootConfig {
+    public static class WebPackage extends RegexPatternTypeFilter {
+        public WebPackage() {
+            super(Pattern.compile("prosayj.thinking.spring4.webmvchelloworld.spittr\\.web"));
+        }
+    }
+}
---
+++ chapter_06/spring4-spring-web-view-rendering-thymeleaf/src/main/java/prosayj/thinking/spring4/webviewthymeleaf/spittr/config/SpitterWebInitializer.java
@@ -0,0 +1,36 @@
+package prosayj.thinking.spring4.webviewthymeleaf.spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import prosayj.thinking.spring4.webviewthymeleaf.spittr.web.WebConfig;
+
+
+/**
+ * SpitterWebInitializer
+ *
+ * @author yangjian
+ */
+public class SpitterWebInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    /**
+     * 指定配置类
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    /**
+     * 将dispactherServlet 映射到 /
+     */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
